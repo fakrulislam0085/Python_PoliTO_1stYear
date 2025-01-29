d935dda29100f07226628e75cb1665adc7812860
Fakrul Islam <fakrulislam0085@gmail.com>
2025-01-29 10:46:11 +0100

    Update alignmentinPython.py

--- LabExercises2.py/alignmentinPython.py
+++ LabExercises2.py/alignmentinPython.py
@@ -1,10 +1,16 @@
 '''
 Here's the basic idea behind aligning text in Python:
-Left alignment: <
-Right alignment: >
-Center alignment: ^
-You can also specify the width of the string, i.e., 
-how many characters wide the field should be.
+
+Left alignment(:<) : When you use left alignment, the text (or number) will be placed starting from the left side of the given width, 
+and any extra space will be added on the right.
+
+Right alignment(:>) : With right alignment, the text (or number) will be placed starting from the right side of the given width, 
+and any extra space will be added on the left.
+
+Center alignment(:^) : With center alignment, the text (or number) will be placed in the middle of the given width, 
+and any extra space will be divided equally on both sides (if possible).
+
+You can also specify the width of the string, i.e., how many characters wide the field should be.
 '''
 
 # Print with alignment
