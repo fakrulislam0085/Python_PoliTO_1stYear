6c2362a898492987396f1c242a2c378a87372f21
Fakrul Islam <fakrulislam0085@gmail.com>
2025-01-29 09:37:27 +0100

    Update 2.1.2_resistances.py

--- LabExercises2.py/2.1.2_resistances.py
+++ LabExercises2.py/2.1.2_resistances.py
@@ -1,13 +1,13 @@
-R1 = int(input("Insert the resistance1: "))
-R2 = int(input("Insert the resistance2: "))
-R3 = int(input("Insert the resistance3: "))
+R1 = float(input("Insert the resistance1: "))
+R2 = float(input("Insert the resistance2: "))
+R3 = float(input("Insert the resistance3: "))
 
 #For series connection 
-R = R1 + R2 + R3
-print("Series resistance: ", R)
+Rs = R1 + R2 + R3
+print("Series resistance: ", Rs, 'Ω')
 
 #For parallel connection
-R = (1/R1) + (1/R2) + (1/R3)
-print("Pallel resistance: ", 1/R)
+Rp = 1((1/R1) + (1/R2) + (1/R3))
+print("Pallel resistance: ", Rp, 'Ω')
 
 
