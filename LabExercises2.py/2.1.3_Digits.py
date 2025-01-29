082c49be287cbe07c7a512ffcb7ebf56cb94019e
Fakrul Islam <fakrulislam0085@gmail.com>
2025-01-29 09:39:24 +0100

    Update 2.1.3_Digits.py

--- LabExercises2.py/2.1.3_Digits.py
+++ LabExercises2.py/2.1.3_Digits.py
@@ -1,6 +1,16 @@
+#OPTION 1: treat the number as a 5-character string
 fiveDigit = int(input("Inser the five digit Number: "))
-
 fiveDigit = str(fiveDigit)  #Conver the number into string 
 
 for indx in range(len(fiveDigit)):
     print(fiveDigit[indx])
+
+
+#OPTION 2: use Division and Modulo of powers of 10
+num = int(input("Enter a 5 digit integer: "))
+
+print((num // 10000) % 10)
+print((num // 1000) % 10)
+print((num // 100) % 10)
+print((num // 10) % 10)
+print(num % 10)
