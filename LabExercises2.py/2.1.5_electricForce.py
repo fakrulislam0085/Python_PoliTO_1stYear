d655f40f34651203a10934728a6551a72d48349c
Fakrul Islam <fakrulislam0085@gmail.com>
2025-01-29 09:50:42 +0100

    Update 2.1.5_electricForce.py

--- LabExercises2.py/2.1.5_electricForce.py
+++ LabExercises2.py/2.1.5_electricForce.py
@@ -1,11 +1,11 @@
-q1 = float(input("insert charge 1: "))
-q2 = float(input("insert charge 2: "))
-r = float(input("Insert the distance between the charges: "))
+from math import pi
 
-constant = 4 * 3.1416 * (8.854 * 1e-12)   #10 to the power negative 12
-calculation1 = q1 * q2 
-calculation2 = r * constant
+constant = 8.854e-12  #10 to the power negative 12
+q1 = float(input("Enter the charge for particle 1(in C): "))
+q2 = float(input("Enter the charge for particle 2(in C): "))
+r = float(input("Insert the distance between the charges(in m): "))
+  
+#compute the force 
+relativeForce = (q1 * q2) / (4 * pi * constant * r ** 2)
 
-relativeForce = calculation1 / calculation2
-
-print(relativeForce)
+print(f"Between the two charges the relative force is: {relativeForce:.2e} N")
